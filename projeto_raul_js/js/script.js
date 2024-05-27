f9f0094fef1c9b4e41f04c419c67cd7634f70751
raul-bonet <161500597+raul-bonet@users.noreply.github.com>
2024-05-27 11:35:19 -0300

    Add files via upload

--- projeto_raul_js/js/script.js
+++ projeto_raul_js/js/script.js
@@ -2,36 +2,28 @@ var meuFormulario = document.getElementById("formulario");
 meuFormulario.addEventListener("submit", function(event) {
   event.preventDefault();
 
-  //Chama a função somar
-  dividir();
+  bissexto();
 
 });
 
 
-//Função somar
-function dividir() {
-  
-  //Captura o que está dentro do primeiro input no formulário
-  //Number - Transforma o texto capturado em Numero
-  let primeiro_numero_inteiro = Number(document.getElementById("primeiro_numero_inteiro").value);
-  
-  //Captura o que está dentro do segundo input no formulário
-  //Number - Transforma o texto capturado em Numero
-  let segundo_numero_inteiro = Number(document.getElementById("segundo_numero_inteiro").value); 
-  
-  //Number - Transforma o texto capturado em Numero
-  let terceiro_numero_inteiro = Number(document.getElementById("terceiro_numero_inteiro").value);
 
-  //Number - Transforma o texto capturado em Numero
-  let quarto_numero_inteiro = Number(document.getElementById("quarto_numero_inteiro").value);
+function bissexto() {
   
-  //Realiza a soma do valor que está na variável "primeiro_numero_inteiro" com o valor que está na
-  //variável "segundo_numero_inteiro"
-  //E a variável "resultado_soma" recebe o valor da soma
-  let resultado_divisao = (primeiro_numero_inteiro + segundo_numero_inteiro + terceiro_numero_inteiro + quarto_numero_inteiro) / 4;
+  
+  let ano = Number(document.getElementById("ano").value);
 
-  //Aqui vai escrever no input "resultado_soma" o valor que está armazenado na variável "resultado_soma"
-  document.getElementById("resultado_divisao").value = resultado_divisao;
+  if (ano%400 == 0)
+    resultado_ano = `O ano ${ano} é bissexto`;
+    else
+    if(ano%4==0 && ano%100!=0)
+      resultado_ano= `O ano ${ano} é bissexto`;
+    else
+      resultado_ano = `O ano ${ano} não é bissexto`;
+    
+
+    document.getElementById("resultado_ano").value = resultado_ano;
+  
   
   
 }
